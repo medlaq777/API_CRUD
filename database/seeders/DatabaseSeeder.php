7a47982d1ba964a1b599318dc4f12797bcc36c68
medlaq777 <m.laqsoumi@gmail.com>
2025-03-19 10:13:33 +0000

    add books factory to seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Books;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        Books::factory(50)->create();
     }
 }
